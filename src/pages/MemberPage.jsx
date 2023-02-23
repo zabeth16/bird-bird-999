2c002945b04e40caa5fab6e200fb5d5050114d66
ZABETH <40664034+zabeth16@users.noreply.github.com>
2023-02-24 02:31:42 +0800

    MemberPage revise: profile photo position caculate; add: upload bird photo buttun

--- src/pages/MemberPage.jsx
+++ src/pages/MemberPage.jsx
@@ -3,8 +3,10 @@ import firebase from 'firebase/compat/app';
 import 'firebaseui/dist/firebaseui.css';
 import React , { useEffect, useState, useRef , useCallback , useMemo }from "react";
 import Base from '../pages/Base.jsx'
+import UploadBird from './page_control/UploadBird.jsx';
 import { MemberPageCSS } from '../css/MemberPage.scss'
 import { getAuth, onAuthStateChanged ,signOut , updateProfile} from "firebase/auth";
+import { getFirestore, doc, setDoc , getDoc , getDocs } from "firebase/firestore"
 import { useNavigate } from "react-router-dom";
 import db from './../../firebase-servise.js';
 import { auth } from './../../firebase-servise.js';
@@ -15,7 +17,6 @@ import { async } from '@firebase/util';
 
 
 const MemberPage = () =>{
-    
     const navigate = useNavigate();//取得 navigate
     const auth = getAuth();    
     const logout = () =>{
@@ -27,6 +28,11 @@ const MemberPage = () =>{
             // An error happened.
             console.log(error)
           });
+    }
+    // 上傳鳥照
+    const [showUpload, setShowUpload] = useState(false);
+    const callUpload = () =>{
+      setShowUpload(true);
     }
     // 會員基本資料
     const [userName , setUserName] = useState("")
@@ -55,6 +61,33 @@ const MemberPage = () =>{
             }
         });
     }, []);
+    // 拿取會員頭貼style設定
+    useEffect(() => {
+      const getStyle = async()=>{
+        try{
+          const docSnap = await getDoc(doc(db, "Profile","user"));
+          if(docSnap) {
+            const left = docSnap.data().user.profileStyle.left
+            const top = docSnap.data().user.profileStyle.top
+            const scaleX = docSnap.data().user.profileStyle.scaleX
+            const scaleY = docSnap.data().user.profileStyle.scaleY
+            const TransformOrigin = docSnap.data().user.profileStyle.transformOrigin
+            setImgPosition({ left: left , top: top})
+            setScaleX(scaleX)
+            setScaleY(scaleY)
+            setTransformOrigin(TransformOrigin)
+            // console.log(TransformOrigin)
+          }
+          else{
+            return
+          }
+        }
+        catch(e){
+          console.log(e.message)
+        }
+      }
+      getStyle();
+    }, []);
 
     // Create a storage reference from our storage service
     const storageRef = ref(storage);  
@@ -66,14 +99,20 @@ const MemberPage = () =>{
     const [scale, setScale] = useState(1); 
     const [scaleX , setScaleX] = useState(1);
     const [scaleY , setScaleY] = useState(1);
-    const [finalPosition , setFialPosition] = useState({ left: 0, top: 0 })
     const canvasRef = useRef(null);
     const [canvasWidth, setCanvasWidth] = useState(200); // 設定 Canvas 寬度為 500px
     const [canvasHeight, setCanvasHeight] = useState(200); // 設定 Canvas 高度為 300px
+    const [showUploadButton , setShowUploadButton] = useState(false)
+    const [uploadStatus, setUploadStatus] = useState(null); // 新增狀態欄位
     // 先選照片
     const handleFileSelect = async(event) => {
         const selectedFile = event.target.files[0];
         setSelectedFile(selectedFile);
+        if(selectedFile){
+          setShowUploadButton(true);
+        }else{
+          setShowUploadButton(false);
+        }
 
         const objectUrl = URL.createObjectURL(selectedFile);
         setPreviewUrl(objectUrl);       
@@ -99,7 +138,7 @@ const MemberPage = () =>{
         const newScaleY = obj.scaleY * obj.canvas.getZoom();
         obj.scaleX = newScaleX;
         obj.scaleY = newScaleY;
-        setScale((newScaleX + newScaleY) / 2);
+        // setScale((newScaleX + newScaleY) / 2);
         setScaleX(newScaleX)
         setScaleY(newScaleY)
         obj.setCoords();
@@ -107,29 +146,40 @@ const MemberPage = () =>{
     }, []); 
     // 預覽拖曳
     const [ newImg , setNewImg] = useState(null)
+    const [profileStyle , setProfileStyle] = useState({
+      "left": 0,
+      "top": 0,
+      "scaleX" :1,
+      "scaleY" : 1
+    })
+    const [ TransformOrigin , setTransformOrigin] = useState('0px, 0px')
     useEffect(() => {
         if (canvasRef.current) {
           const canvas = new fabric.Canvas(canvasRef.current);
           canvas.width = canvasWidth;
           canvas.height = canvasHeight;
-          if (image) {
+          if (image) {           
             const img = new fabric.Image(image, {
               left: 0,
               top: 0,
-              scaleX: canvas.width / image.width,
-              scaleY: canvas.height / image.height,
+              scaleX:  canvas.width / image.width,
+              scaleY:  canvas.height / image.height,
             });
+
     
             canvas.add(img);
             canvas.setActiveObject(img);
             img.on('scaling', handleScaling);
             setNewImg(img)
+     
           }
     
           canvas.on('object:moving', (e) => {
             const obj = e.target;
             obj.setCoords();
-            setImgPosition({ left: obj.left, top: obj.top });
+            // setImgPosition({ left: obj.left, top: obj.top });
+            const boundingRect = obj.getBoundingRect();
+             console.log(obj)          
           });
     
           canvas.on('object:scaling', (e) => {
@@ -137,6 +187,68 @@ const MemberPage = () =>{
             obj.setCoords();           
             // 有所謂的obj.scaleX 、 obj.scaleY、obj.left、obj.top
             // 可以console.log(obj)來看
+            const scaleX = obj.scaleX * (obj.width / canvasWidth);
+            const scaleY = obj.scaleY * (obj.height / canvasHeight);
+            setScaleX(scaleX);
+            setScaleY(scaleY);
+            const boundingRect = obj.getBoundingRect();
+            const left = (boundingRect.left + obj.width / 2) * obj.scaleX - canvasWidth / 2;
+            const top = (boundingRect.top + obj.height / 2) * obj.scaleY - canvasHeight / 2;
+            // 計算出圖像的中心點坐標
+            const centerX = boundingRect.left + obj.width / 2;
+            const centerY = boundingRect.top + obj.height / 2;
+            // 計算出圖像相對於 canvas-container 的偏移量
+            const leftMove = centerX * obj.scaleX - canvasWidth / 2;
+            const topMove = centerY * obj.scaleY - canvasHeight / 2;
+            // 計算出圖像的縮放中心點坐標
+            const tx = (obj.left -left) / obj.scaleX;
+            const ty = (obj.top - top) / obj.scaleY;
+            // 計算出圖像在 canvas-container 中的位置
+            const containerLeft = 0 + left + obj.left;
+            const containerTop = 0 + top + obj.top;
+           
+            // 計算 transformOrigin 的值 長高圖
+            if (obj.height > obj.width){
+              
+              const adjustLeft =canvasWidth/2
+              const adjustTop =canvasHeight/2
+              // const tx =  ( (obj.width / canvasWidth) / 2) * scaleX + obj.left ; //boundingRect.left +
+              // const ty =  ((obj.height / canvasHeight) / 2) * scaleY + obj.top; // boundingRect.top
+           
+              setTransformOrigin(tx , ty);
+              setProfileStyle({
+                "left": containerLeft,
+                "top": containerTop,
+                "scaleX" :scaleX,
+                "scaleY" : scaleY,
+                "transformOrigin":  tx + 'px ' + ty + 'px'  // tx + 'px ' + ty + 'px'
+              })
+            }
+            else{           
+              obj.setCoords();     
+              // const tx = ((obj.width / canvasWidth)) * obj.scaleX + obj.left  + (canvasWidth / 2) ; //boundingRect.left +
+              // const ty = ( (obj.height / canvasHeight)) * obj.scaleY + obj.top;         
+              console.log("中心left",left,"top: ", top)
+              const adjustLeft =canvasWidth/2
+              const adjustTop =canvasHeight/2
+              setTransformOrigin(tx , ty);
+              setProfileStyle({
+                "left": containerLeft,
+                "top": containerTop,
+                "scaleX" :scaleX,
+                "scaleY" : scaleY,
+                "transformOrigin": tx + 'px ' + ty + 'px'
+              })
+            }
+            
+            // console.log(TransformOrigin)
+            // setScaleX(obj.scaleX)
+            // setScaleY(obj.scaleY)
+           
+
+            // console.log(obj)
+            // console.log( "左" ,boundingRect.left)
+            // console.log("top" ,boundingRect.top)
           });
 
 
@@ -144,12 +256,13 @@ const MemberPage = () =>{
       }, [image, handleScaling]);
     
     // 確定上傳
-    const handleUpload = () => {
+
+    const handleUpload = async() => {
         // console.log(userId)
         if (!selectedFile) {
           return;
-        }        
-
+        }
+        setUploadStatus("上傳中");
         // 獲取當前圖像的位置和scale
         const canvas = canvasRef.current;
         // 移除事件监听器
@@ -161,11 +274,24 @@ const MemberPage = () =>{
         
           // 在Firebase Storage上傳前，將位置和scale信息寫入圖像元數據
           // 用database吧
-          const metadata = { customMetadata: { imgPosition, scale } };
-          console.log(metadata)
+          try {
+            // 把最終大頭照設定上傳到database
+
+            await setDoc(doc(db, "Profile", "user"), {
+              user:{
+                "email": userEmail,
+                profileStyle
+              }               
+              
+            });
+          } catch (err) {
+            console.error("Error: ", err);
+          }
+
           const uploadPath = `users/${userEmail}/profilePhoto.png`;        
           uploadBytes(ref(storage, uploadPath), selectedFile ).then(() => {
-            console.log('照片上傳成功');
+            setUploadStatus("上傳成功，自動刷新頁面");
+            // console.log('照片上傳成功');
               // 照片上傳成功後，獲取照片下載 URL 並顯示照片
               getDownloadURL(ref(storage, uploadPath)).then((url) => {
                   // Update the user profile in Firebase Auth
@@ -192,26 +318,40 @@ const MemberPage = () =>{
     return (      
         <div>
             <Base></Base>
-            <div className='main-member'>一個會員頁
-                <div className='user-box'>
+            {showUpload && <UploadBird />}
+            <div className='main'>
+            
+            <div className='main-member'> {/* 鳥友會員頁 */} 
+                <div className='bird-section'>
+                  <button className='upload-bird' onClick={callUpload}>上傳鳥照</button>
+                  
+                </div>
+                
+                <div className='user-box'> 
                     <div className='user-photo-container'>
                         {!previewPhoto ? 
                         (
-                            <>
-                                                        
+                            <>                                                        
                             <canvas className='canvas' ref={canvasRef} width={canvasWidth} height={canvasHeight}/>
-                            {/* <img src={previewUrl} className='photo-preview' />   */}
-                                                    
+                            {/* <img src={previewUrl} className='photo-preview' />   */}                                                    
                             </>
                         )
                             :
-                            (
+                        (
                             <>                            
                              <img src={userPhoto}  className='user-photo'    
                              style={{
+                              width: '200px',
+                              height:'200px',
                               left: imgPosition.left,
                               top: imgPosition.top,
-                              transform: `scaleX(${scaleX}) scaleY(${scaleY})`}}                        
+                              position: 'relative', 
+                              // transformOrigin: TransformOrigin,
+                              // transformStyle: 'initial',    
+                              transform: `scaleX(${scaleX}) scaleY(${scaleY})`,
+                              
+                            }}
+                                            
                              /> {/*className='user-photo'*/}
                             
                             </>
@@ -221,21 +361,30 @@ const MemberPage = () =>{
                        
                     </div>
                     <label htmlFor="upload-photo" className='upload-photo'>
-                        <span className='choose'>更換照片</span>                        
+                        <span className='choose'>{uploadStatus || '更換照片'}</span>                      
                         <input
                         type="file"
                         id="upload-photo"
                         onChange={handleFileSelect}
                         style={{ display: "none" }}
                         />
-                        <button onClick={handleUpload}>上傳 !</button>
+                        {showUploadButton &&
+                         <button onClick={handleUpload}>上傳 !</button>
+                        }
+                        
                     </label>
                     
                 </div>
-                <div className='user-name'>{userName}</div>                
-                <button onClick={logout} className='logout'> 點此登出 </button>
+                <div className='user-name'>{userName}</div>  
+                <div className='logout-box'>
+                  <button onClick={logout} className='logout'> 點此登出 </button>
+                </div>
+                
+                          
+                
             </div>
         
+        </div>
         </div>
     )
 
